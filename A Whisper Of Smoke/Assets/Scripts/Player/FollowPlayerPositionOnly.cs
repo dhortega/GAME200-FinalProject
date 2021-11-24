0d949268de0656a97fd3c9e4f4899524df9a2e64
Jessica Wei <jessicawei9@gmail.com>
2021-11-23 18:24:53 -0800

    + Player left right movement complete, working on camera movement and jump next

---
+++ A Whisper Of Smoke/Assets/Scripts/Player/FollowPlayerPositionOnly.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerPositionOnly : MonoBehaviour
+{
+    private bool isFacingLeft = false;
+
+    [SerializeField] private GameObject player;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        if (player.GetComponent<PlayerMovement>().IsFacingLeft() && !isFacingLeft|| !player.GetComponent<PlayerMovement>().IsFacingLeft() && isFacingLeft) {
+            isFacingLeft = !isFacingLeft;
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y * -1, transform.localEulerAngles.z);
+        }
+        transform.position = player.transform.position;
+    }
+
+    public Animator GetAnimator() {
+        return anim;
+    }
+}
---
+++ A Whisper Of Smoke/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private bool isFacingLeft = false;
+    [SerializeField] private Animator anim;
+    private Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool playerXInput = Input.GetButton("Horizontal");
+        if (playerXInput)
+        {
+            float xAxisValue = Input.GetAxis("Horizontal");
+            if (anim.GetBool("isIdle"))
+                anim.SetBool("isIdle", false);
+            rb.velocity = new Vector3(speed * xAxisValue, 0.0f, 0.0f);
+            if (xAxisValue < 0 && !isFacingLeft)
+            {
+                isFacingLeft = true;
+            }
+            else if (xAxisValue > 0 && isFacingLeft)
+            {
+                isFacingLeft = false;
+            }
+        }
+        else if (!playerXInput && rb.velocity.x != 0.0f)
+        {
+            rb.velocity = new Vector3(0.0f, rb.velocity.y, rb.velocity.z);
+            if (!anim.GetBool("isIdle")) {
+                anim.SetBool("isIdle", true);
+            }
+        }
+    }
+
+    public bool IsFacingLeft() {
+        return isFacingLeft;
+    }
+}
