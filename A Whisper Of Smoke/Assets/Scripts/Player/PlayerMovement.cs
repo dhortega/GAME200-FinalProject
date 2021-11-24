d7495221c5c58a5fb748e897619f5a6f129ee995
Jessica Wei <jessicawei9@gmail.com>
2021-11-24 00:38:33 -0800

    + Added cinemachine camera movement + Added jump (also shifted all physics movement into FixedUpdate) + Added wing animations and reanimated new model

--- A Whisper Of Smoke/Assets/Scripts/Player/PlayerMovement.cs
+++ A Whisper Of Smoke/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,46 +5,93 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 200.0f;
     [SerializeField] private bool isFacingLeft = false;
     [SerializeField] private Animator anim;
     private Rigidbody rb;
+    private bool canJump = false;
+
+    private bool askedToJump = false;
+    private bool askedToUpdateMovement = false;
+    private Vector3 updateToThisVelocity;
     
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region PHYSICS-BASED HORIZONTAL MOVEMENT
         bool playerXInput = Input.GetButton("Horizontal");
         if (playerXInput)
         {
             float xAxisValue = Input.GetAxis("Horizontal");
-            if (anim.GetBool("isIdle"))
-                anim.SetBool("isIdle", false);
-            rb.velocity = new Vector3(speed * xAxisValue, 0.0f, 0.0f);
+            //rb.velocity = new Vector3(speed * xAxisValue, rb.velocity.y, rb.velocity.z);
+            updateToThisVelocity = new Vector3(speed * xAxisValue, rb.velocity.y, rb.velocity.z);
+            askedToUpdateMovement = true;
             if (xAxisValue < 0 && !isFacingLeft)
             {
                 isFacingLeft = true;
+                anim.SetBool("isIdle", true);
             }
             else if (xAxisValue > 0 && isFacingLeft)
             {
                 isFacingLeft = false;
-            }
-        }
-        else if (!playerXInput && rb.velocity.x != 0.0f)
-        {
-            rb.velocity = new Vector3(0.0f, rb.velocity.y, rb.velocity.z);
-            if (!anim.GetBool("isIdle")) {
                 anim.SetBool("isIdle", true);
             }
+            else {
+                if (anim.GetBool("isIdle"))
+                    anim.SetBool("isIdle", false);
+            }
+        }
+        if (!playerXInput && rb.velocity.x != 0.0f && !askedToUpdateMovement)
+        {
+            //rb.velocity = new Vector3(0.0f, rb.velocity.y, rb.velocity.z); 
+            updateToThisVelocity =  new Vector3(0.0f, rb.velocity.y, rb.velocity.z);
+            askedToUpdateMovement = true;
+            if (!anim.GetBool("isIdle"))
+            {
+                anim.SetBool("isIdle", true);
+            }
+        }
+        #endregion
+
+        #region PHYSICS-BASED VERTICAL MOVEMENT (single jump)
+        bool playerYInput = Input.GetButtonDown("Jump");
+        if (playerYInput && canJump) {
+            canJump = false;
+            askedToJump = true;
+            anim.SetBool("isMidair", true);
+        }
+        #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        if (askedToJump) {
+            rb.AddForce(Vector3.up * jumpForce);
+            askedToJump = false;
+        }
+        if (askedToUpdateMovement) {
+            rb.velocity = updateToThisVelocity;
+            askedToUpdateMovement = false;
         }
     }
 
     public bool IsFacingLeft() {
         return isFacingLeft;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor") && !canJump) {
+            anim.SetBool("isMidair", false);
+            canJump = true;
+        }
     }
 }
