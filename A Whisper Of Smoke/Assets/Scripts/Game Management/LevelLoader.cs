4f9a5270c16647368b39b2dac8bfa1d54d9f3ec4
Jessica Wei <jessicawei9@gmail.com>
2021-11-27 01:46:11 -0800

    + Added beginning cutscene and a flexible dialogue system for static narrative scenes + Added LevelLoader to handle scene transitions (fade in , fade out) ~ IP good end cutscene, subtitles for purifying forgotten memories

---
+++ A Whisper Of Smoke/Assets/Scripts/Game Management/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float fadeDuration = 1.0f;
+    private Animator anim;
+
+    private void Start() {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void LoadScene(int sceneIndex) {
+        StartCoroutine(LoadSceneEnumerator(sceneIndex));
+    }
+
+    IEnumerator LoadSceneEnumerator(int sceneIndex) {
+        anim.SetTrigger("fadeOut");
+        yield return new WaitForSeconds(fadeDuration);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ A Whisper Of Smoke/Assets/Scripts/Narrative/SceneNarration.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneNarration : MonoBehaviour
+{
+    [SerializeField] private List<string> quotesToDisplay;
+    [SerializeField] private List<float> timeForEachDisplay;
+    [SerializeField] private float fadeAnimationTime = 1.0f;
+    [SerializeField] private int nextSceneIndex;
+    [SerializeField] private LevelLoader levelLoader;
+    private Animator anim;
+    private Text textbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        textbox = gameObject.GetComponent<Text>();
+        if (quotesToDisplay.Count == timeForEachDisplay.Count && anim != null && textbox != null)
+            StartCoroutine( PlayCutscene());
+        else
+            Debug.Log("Double check your game object! Something went wrong..");
+    }
+
+    IEnumerator PlayCutscene() {
+        //yield return new WaitForSeconds(fadeAnimationTime);
+        for (int i = 0; i < quotesToDisplay.Count; ++i) {
+            textbox.text = quotesToDisplay[i];
+            anim.SetBool("isVisible", true);
+            yield return new WaitForSeconds(timeForEachDisplay[i]);
+            anim.SetBool("isVisible", false);
+            yield return new WaitForSeconds(fadeAnimationTime);
+        }
+        levelLoader.LoadScene(nextSceneIndex);
+    }
+}
