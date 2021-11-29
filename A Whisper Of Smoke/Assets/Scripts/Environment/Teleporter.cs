e43d536f854fe5b01d38b6ba6f944be5d3072b7a
Mia <alicie73@yahoo.com>
2021-11-29 00:44:03 -0800

    Basic teleportation
    Do we want actual teleport assets? Teleportation works but could be a smoother transition.

---
+++ A Whisper Of Smoke/Assets/Scripts/Environment/Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = teleportTarget.transform.position;
+    }
+}
