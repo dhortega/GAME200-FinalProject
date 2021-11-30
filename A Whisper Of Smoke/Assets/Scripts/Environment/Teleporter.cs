b269c53523b5fae9bc59848170c67cf7ea4514ed
Jessica Wei <jessicawei9@gmail.com>
2021-11-30 14:54:54 -0800

    + Added teleporters to GameScene + Smoothed camera movement on teleport

--- A Whisper Of Smoke/Assets/Scripts/Environment/Teleporter.cs
+++ A Whisper Of Smoke/Assets/Scripts/Environment/Teleporter.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Teleporter : MonoBehaviour
 {
     public Transform teleportTarget;
     public GameObject Player;
+    public CinemachineVirtualCamera playerCamera;
 
     private void OnTriggerEnter(Collider other)
     {
+        Vector3 initialPosition = Player.transform.position;
+        
         Player.transform.position = teleportTarget.transform.position;
+        playerCamera.OnTargetObjectWarped(Player.transform, initialPosition - teleportTarget.transform.position);
+        playerCamera.PreviousStateIsValid = false;
     }
 }
