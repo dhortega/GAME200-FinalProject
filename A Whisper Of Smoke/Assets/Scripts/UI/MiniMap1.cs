00aae6ec78062403871cf5271ad7e8c61ea070a0
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2021-12-05 21:39:44 -0800

    Created Minimap Zoomed Out and Zoomed In
    - Need to create masking then revealing

---
+++ A Whisper Of Smoke/Assets/Scripts/UI/MiniMap1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap1 : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector2 TransformPosition(Vector3 position)
+    {
+        Vector3 offset = position - target.position;
+
+        Vector2 newPosition = new Vector2(offset.x, offset.z);
+
+        return newPosition;
+    }
+}
---
+++ A Whisper Of Smoke/Assets/Scripts/UI/Minimap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+
+    private float xOffset;
+    private float yOffset;
+    private float zOffset;
+
+    private void Start()
+    {
+        xOffset = transform.position.x - player.position.x;
+        yOffset = transform.position.y - player.position.y;
+        zOffset = transform.position.z - player.position.z;
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position = new Vector3(
+            player.position.x + xOffset,
+            player.position.y + yOffset,
+            player.position.z + zOffset);
+    }
+    //private void LateUpdate()
+    //{
+    //    Vector3 newPosition = player.position;
+    //    //newPosition.y = transform.position.y;
+    //    transform.position = newPosition;
+    //}
+
+}
